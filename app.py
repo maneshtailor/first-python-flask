277d7da3c088b41c6a01a7202499b46baffb7f7a
Manesh Tailor <manesh_tailor@hotmail.com>
2022-01-28 08:14:38 +0000

    Includes files to run in AWS Lightsail

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return 'Hello, Flask Mtailor!'
+def hello_world():
+   return "Hello, World!"
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
